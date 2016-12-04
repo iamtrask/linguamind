78f8a83d7dfa7017c357e7424f1f80729887d6e8
Amber Edmundson <amberedmundson@Ambers-MacBook-Air.local>
2016-12-04 20:19:16 +0000

    removed build files

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@ export CXX=/usr/bin/clang++
 
 rm -rf build
 rm example.py
+rm example.pyc
+rm example_wrap.c
 rm example_wrap.cxx
 rm _example.so
 
-swig -python example.i
-swig -c++ -python example.i
+# swig -python example.i
+# swig -c++ -python example.i
 
-# python setup.py build_ext --inplace
-sudo python setup.py install
+# # python setup.py build_ext --inplace
+# sudo python setup.py install
