1dd1bd391027ea773acc0a69c158266943d7d653
Amber Edmundson <amberedmundson@Ambers-MacBook-Air.local>
2016-12-04 20:14:08 +0000

    boilerplate swig python code and build file for OSX

---
+++ build.sh
@@ -0,0 +1,13 @@
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
+rm -rf build
+rm example.py
+rm example_wrap.cxx
+rm _example.so
+
+swig -python example.i
+swig -c++ -python example.i
+
+# python setup.py build_ext --inplace
+sudo python setup.py install
