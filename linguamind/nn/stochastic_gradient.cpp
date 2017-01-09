78ed2f8a12e476af8e455eb7f43e6002edbb6006
Amber Edmundson <amberedmundson@Ambers-MacBook-Air.local>
2017-01-09 18:34:04 +0000

    added new notebook for word2vec stuff

--- linguamind/nn/stochastic_gradient.cpp
+++ linguamind/nn/stochastic_gradient.cpp
@@ -8,7 +8,7 @@ StochasticGradient::StochasticGradient(Sequential* mlp, MSECriterion* criterion)
 float StochasticGradient::train(std::vector<int> input_indices, std::vector<int> output_indices, Vector* target_values, float alpha, int iterations) {
 	for(int iter=0; iter<iterations; iter++) {
 		Vector* pred = this->mlp->forward(input_indices,output_indices);
-		// float error = this->criterion->forward(pred, target_values,output_indices);
+		float error = this->criterion->forward(pred, target_values,output_indices);
 		this->mlp->backward(this->criterion->backward(pred,target_values,output_indices),output_indices);
 
 		Vector* prev_layer_output = NULL;
@@ -19,5 +19,5 @@ float StochasticGradient::train(std::vector<int> input_indices, std::vector<int>
 
 		this->mlp->get(this->mlp->layers.size()-1)->accGradParameters(this->mlp->get(this->mlp->layers.size()-2)->getOutput(),criterion->grad,alpha);
 	}
-	return 0;
+	return error;
 }
